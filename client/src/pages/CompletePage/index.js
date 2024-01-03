661c4dbecea15fd26bce3211fa193cdc8e38d677
jngmnj <exorbitance@naver.com>
2024-01-04 01:37:00 +0900

    [ADD] 끝!!!

--- client/src/pages/CompletePage/index.js
+++ client/src/pages/CompletePage/index.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { OrderContext } from '../../context/OrderContext'
+import axios from 'axios';
 
-const CompletePage = () => {
-  return (
-    <div>CompletePage</div>
-  )
+const CompletePage = ({ setStep }) => {
+  const [orderHistory, setOrderHistory] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [orderData] = useContext(OrderContext);
+
+  useEffect(() => {
+    orderComplete(orderData)
+  }, [orderData]);
+
+  const orderComplete = async (orderData) => {
+    try {
+      const response = await axios.post('http://localhost:4000/order',orderData)
+      setOrderHistory(response.data)
+      setLoading(false)
+    } catch (e) {
+      console.log(e)
+      setLoading(false)
+    }
+  }
+  
+  if(loading) {
+    return <div> loading... </div>
+  } else {
+    return (
+      <div style={{ textAlign: 'center'}}>
+        <h1>주문이 완료되었습니다.</h1>
+        <h3>주문내역</h3>
+        <table style={{ margin: 'auto', border: '1px solid black'}}>
+          <tr>
+            <th>주문번호</th>
+            <th>주문가격</th>
+          </tr>
+          {orderHistory.map((item) => (
+            <tr key={item.orderNumber}>
+              <td>{item.orderNumber}</td>
+              <td>{item.price}</td>
+            </tr>
+          ))}
+        </table>
+        <br />
+        <button onClick={() => setStep(0)}>첫 페이지로</button>
+      </div>
+    )
+  }
 }
 
 export default CompletePage
--- client/src/pages/SummaryPage/index.js
+++ client/src/pages/SummaryPage/index.js
@@ -1,10 +1,46 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { OrderContext } from "../../context/OrderContext"
 
 const SummaryPage = ({ setStep }) => {
   const [checked, setChecked] = useState(false);
+  const [orderDetails] = useContext(OrderContext)
+
+  const productArray = Array.from(orderDetails.products);
+  const productList = productArray.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ))
+
+  // option
+  const hasOptions = orderDetails.options.size > 0;
+  let optionList = null;
+  if(hasOptions) {
+    const optionArray = Array.from(orderDetails.options.keys()); // key만 빼기
+    optionList = optionArray.map((key) => <li key={key}>{key}</li>);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStep(2);
+  }
   return (
     <div>
-      <form>
+      <h1>주문 확인</h1>
+      <h2>여행 상품: {orderDetails.totals.products}</h2>
+      
+      <ul>
+        {productList}
+      </ul>
+      {hasOptions && (
+        <>
+          <h2>옵션: {orderDetails.totals.options}</h2>
+          <ul>
+            {optionList}
+          </ul>
+        </>
+      )}
+      <form onSubmit={handleSubmit}>
         <input
           type="checkbox"
           defaultChecked={checked}
@@ -13,9 +49,7 @@ const SummaryPage = ({ setStep }) => {
         />{" "}
         <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
         <br />
-        <button disabled={!checked} type="submit"
-          onClick={() => setStep(2)}
-        >
+        <button disabled={!checked} type="submit">
           주문확인
         </button>
       </form>
